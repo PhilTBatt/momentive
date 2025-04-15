f4bcf446c527325642c36c9499a24cb1b9047e4c
Philip Battersby <philtbatt@gmail.com>
2025-04-15 14:21:36 +0100

    puts event

--- src/app/api/events/[id]/route.ts
+++ src/app/api/events/[id]/route.ts
@@ -18,3 +18,24 @@ export async function DELETE({ params }: { params: Promise<{ id: string }> }) {
 
   	return new Response(null, { status: 201, headers: { 'Content-Type': 'application/json' } })
 }
+
+export async function PUT(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+	const { id } = await params
+	const body = await request.json()
+    const { title, description, date, location } = body
+
+    for (const field of [title, description, date, location]) {
+        if (!field) 
+            return new Response(JSON.stringify({ error: 'Field is missing' }), { status: 400 })
+
+        if (typeof field !== 'string') 
+            return new Response(JSON.stringify({ error: 'Invalid input' }), { status: 400, headers: { 'Content-Type': 'application/json' }})
+    }
+
+	const result = await db`UPDATE users SET title = ${title}, description = ${description}, date = ${date}, 
+		location = ${location} WHERE id = ${id} RETURNING id, name, email`
+
+	if (result.length === 0) return new Response("Event not found", { status: 404 })
+
+	return new Response(JSON.stringify(result[0]), { status: 200, headers: { 'Content-Type': 'application/json' } })
+}
--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -21,7 +21,6 @@ export async function DELETE({ params }: { params: Promise<{ id: string }> }) {
 
 export async function PUT(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
   	const { id } = await params
-  
   	const body = await request.json()
   	const { name, email } = body
 
