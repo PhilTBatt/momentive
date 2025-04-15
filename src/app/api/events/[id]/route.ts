77c3b36319956661972c588e7c8be3d31ecb7eeb
Philip Battersby <philtbatt@gmail.com>
2025-04-15 13:36:49 +0100

    gets event

--- src/app/api/events/[id]/route.ts
+++ src/app/api/events/[id]/route.ts
@@ -1,9 +1,13 @@
+import { db } from "@/utils/connection";
 import { NextRequest } from "next/server";
 
-export async function GET(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
-  	const id = (await params).id
-  	// Fetch data
-  	return new Response(JSON.stringify(id), { status: 200, headers: { 'Content-Type': 'application/json' } })
+export async function GET({ params }: { params: Promise<{ id: string }> }) {
+  	const { id } = await params
+  	const event = await db`SELECT * FROM events WHERE id = ${id}`
+
+	if (!event) return new Response('Event not found', { status: 404 })
+
+  	return new Response(JSON.stringify({ event }), { status: 200, headers: { 'Content-Type': 'application/json' } })
 }
    
 export async function POST(request: NextRequest) {
