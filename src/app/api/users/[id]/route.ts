7d374e61c8115a0dc52cf03ba198d852999b6b52
Philip Battersby <philtbatt@gmail.com>
2025-04-15 13:09:16 +0100

    deletes user

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -8,3 +8,12 @@ export async function GET({ params }: { params: Promise<{ id: string }> }) {
   
   return new Response( JSON.stringify({ user }), { status: 200,  headers: { 'Content-Type': 'application/json' } })
 }
+
+export async function DELETE({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params
+  const user = await db`SELECT * FROM users WHERE id = ${id}`
+
+  if (!user) return new Response('User not found', { status: 404 })
+    
+  return new Response(null, { status: 204 })
+}
