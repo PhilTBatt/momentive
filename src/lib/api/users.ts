6ef630cd61c692b5cbf13e58116ebe4815468f6d
Philip Battersby <philtbatt@gmail.com>
2025-04-16 22:33:19 +0100

    adds functions to interact with users api

--- src/lib/api/users.ts
+++ src/lib/api/users.ts
@@ -1,54 +1,36 @@
+import axios from 'axios'
+
 interface User {
-	id: string;
-	name: string;
-	email: string;
+	id: string
+	name: string
+	email: string
 }
-  
+
 export async function getUsers({ order, limit, page }: { order?: string; limit?: number; page?: number }): Promise<User[]> {
-	const response = await fetch(`/api/users?order=${order}&limit=${limit}&page=${page}`);
-	const data = await response.json()
-	return data.users;
+	const response = await axios.get('/api/users', { params: { order, limit, page } })
+	return response.data.users
 }
-  
+
 export async function getUserById(id: string): Promise<User> {
-	const response = await fetch(`/api/users/${id}`)
-	const data = await response.json()
-	return data.user
+	const response = await axios.get(`/api/users/${id}`)
+	return response.data.user
 }
-  
+
 export async function postNewUser({ name, email }: { name: string; email: string }): Promise<User> {
-	const response = await fetch('/api/users', {
-	  	method: 'POST',
-	  	headers: {
-			'Content-Type': 'application/json'
-	  	},
-	  	body: JSON.stringify({ name, email })
-	})
-	const data = await response.json()
-	return data.user
+	const response = await axios.post('/api/users', { name, email })
+	return response.data.user
 }
-  
+
 export async function updateUser(id: string, { name, email }: { name: string; email: string }): Promise<User> {
-	const response = await fetch(`/api/users/${id}`, {
-	  	method: 'PATCH',
-	  	headers: {
-			'Content-Type': 'application/json'
-	  	},
-	  	body: JSON.stringify({ name, email })
-	})
-
-	const data = await response.json()
-	return data.user
+	const response = await axios.patch(`/api/users/${id}`, { name, email })
+	return response.data.user
 }
-  
-export async function deleteUser(id: string): Promise<{ status: number; msg: string }> {
-	const response = await fetch(`/api/users/${id}`, {
-	  	method: 'DELETE'
-	})
 
-	if (response.status === 204) {
-	  	return { status: 204, msg: 'User deleted successfully' }
-	} else {
-	  	throw new Error('Failed to delete user')
+export async function deleteUser(id: string): Promise<{ status: number; msg: string }> {
+	try {
+		await axios.delete(`/api/users/${id}`)
+		return { status: 204, msg: 'User deleted successfully' }
+	} catch (err: any) {
+		return { status: err.status, msg: err.msg }
 	}
-}  
+}
