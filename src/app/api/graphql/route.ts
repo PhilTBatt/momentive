b7f70cd764a8f45b123c514ddbbf875c7fe190d0
PhilTBatt <philtbatt@gmail.com>
2025-06-30 01:10:44 +0100

    apollo server

---
+++ src/app/api/graphql/route.ts
@@ -0,0 +1,14 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "../../graphql/schema";
+
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs })
+  const { url } = await startStandaloneServer(server)
+  console.log(`
+    🚀  Server is running!
+    📭  Query at ${url}
+  `)
+}
+
+startApolloServer()
