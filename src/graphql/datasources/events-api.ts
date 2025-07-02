e2731acda4eb32b8a2cb18dfe08f524006357c27
PhilTBatt <philtbatt@gmail.com>
2025-07-03 00:21:24 +0100

    adds apollo datasources

---
+++ src/graphql/datasources/events-api.ts
@@ -0,0 +1,38 @@
+import { RESTDataSource } from '@apollo/datasource-rest'
+import { Event, CreateEventInput } from '../__generated__/types'
+
+export class EventAPI extends RESTDataSource {
+  baseURL = 'http://localhost:3000/api/'
+
+  async getEvents(params?: Partial<{
+    sortBy: string
+    order: string
+    topic: string
+    userId: string
+    limit: number
+    page: number
+  }>): Promise<{ events: Event[] }> {
+    const stringParams = new URLSearchParams(Object.entries(params ?? {}).map(([k, v]) => [k, String(v)]))
+    return this.get('events', {params: stringParams})
+  }
+
+  async getEvent(id: string): Promise<{ event: Event }> {
+    return this.get(`events/${id}`)
+  }
+
+  async createEvent(input: CreateEventInput): Promise<{ event: Event }> {
+    return this.post('events', { body: input })
+  }
+
+  async updateEvent(id: string, input: Partial<Omit<CreateEventInput, 'createdBy'>>): Promise<{ event: Event }> {
+    return this.patch(`events/${id}`, { body: input })
+  }
+
+  async deleteEvent(id: string): Promise<{ msg: string }> {
+    return this.delete(`events/${id}`)
+  }
+
+  async addAttendee(eventId: string, input: { name: string; email: string }): Promise<{ event: Event }> {
+      return this.post(`events/${eventId}/attendees`, { body: input })
+  }
+}
---
+++ src/graphql/datasources/users-api.ts
@@ -0,0 +1,39 @@
+import { RESTDataSource } from '@apollo/datasource-rest'
+import { User, CreateUserInput } from '../__generated__/types'
+
+export class UserAPI extends RESTDataSource {
+  baseURL = 'http://localhost:3000/api/'
+
+  async getUsers(params?: Partial<{
+    order: string
+    limit: number
+    page: number
+  }>): Promise<{ users: User[] }> {
+    const stringParams = new URLSearchParams(Object.entries(params ?? {}).map(([k, v]) => [k, String(v)]))
+    return this.get('users', {params: stringParams})
+  }
+
+  async getUser(id: string): Promise<{ user: User }> {
+    return this.get(`users/${id}`)
+  }
+
+  async getUserByEmail(email: string): Promise<{ user: User }> {
+    return this.post('users/email', { body: { email } })
+  }
+
+  async createUser(input: CreateUserInput): Promise<{ user: User }> {
+    return this.post('users', { body: input })
+  }
+
+  async updateUser(id: string, input: Partial<CreateUserInput>): Promise<{ user: User }> {
+    return this.patch(`users/${id}`, { body: input })
+  }
+
+  async deleteUser(id: string): Promise<{ msg: string }> {
+    return this.delete(`users/${id}`)
+  }
+
+  async signIn(input: { id: string; email: string; password: string }): Promise<boolean> {
+    return this.post('users/signin', { body: input })
+  }
+}
