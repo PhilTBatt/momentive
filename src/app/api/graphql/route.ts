cf303dd6dc8753c255ad1b5a06c64475a56664fc
PhilTBatt <philtbatt@gmail.com>
2025-07-29 21:02:12 +0100

    adds type to server handler

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -4,13 +4,15 @@ import { typeDefs } from "../../../graphql/schema";
 import { resolvers } from "../../../graphql/resolvers";
 import { EventAPI } from "@/graphql/datasources/events-api";
 import { UserAPI } from "@/graphql/datasources/users-api";
+import { NextRequest } from "next/server";
 
 const server = new ApolloServer({ typeDefs, resolvers })
 
-const handler = startServerAndCreateNextHandler(server, {
-  context: async () => {
+const handler = startServerAndCreateNextHandler<NextRequest>(server, {
+  context: async (req) => {
     const { cache } = server
     return {
+      req,
       dataSources: {
         eventAPI: new EventAPI({ cache }),
         userAPI: new UserAPI({ cache })
